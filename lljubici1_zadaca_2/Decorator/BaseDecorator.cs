654ef85759a73b5c713191652493d73c8c2491d9
Luka Ljubičić <lljubici1@foi.hr>
2019-11-30 16:43:28 +0100

    Observer zapocet

---
+++ lljubici1_zadaca_2/Decorator/BaseDecorator.cs
@@ -0,0 +1,20 @@
+﻿using lljubici1_zadaca_2.Composite;
+using System;
+using System.Collections.Generic;
+
+namespace lljubici1_zadaca_2.Decorator
+{
+    public class BaseDecorator : IComponent
+    {
+        private IComponent wrappee;
+        public BaseDecorator(IComponent wrappee)
+        {
+            this.wrappee = wrappee;
+        }
+
+        public List<IRasporedProgramaComponent> IspisVremenskogPlana()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lljubici1_zadaca_2/Observer/IObserver.cs
@@ -0,0 +1,7 @@
+﻿namespace lljubici1_zadaca_2.Observer
+{
+    interface IObserver
+    {
+        void Azuriraj(ISubject subject);
+    }
+}
