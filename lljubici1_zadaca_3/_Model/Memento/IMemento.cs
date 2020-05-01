13279003b2bc4f0274d63cd9ef48aa1355cb82cf
Luka Ljubičić <lljubici1@foi.hr>
2020-05-01 17:27:42 +0200

    Initial commit

--- lljubici1_zadaca_3/_Model/Memento/IMemento.cs
+++ lljubici1_zadaca_3/_Model/Memento/IMemento.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using lljubici1_zadaca_3._Model.Composite;
+using System;
 using System.Collections.Generic;
-using lljubici1_zadaca_3._Model.Composite;
 
 namespace lljubici1_zadaca_3._Model.Memento
 {
